1660383348e4fb96cf4c9550ba49343bf6a6e3b5
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-11-19 16:18:52 +0100

    completed the forth exercise of variables, define-basic-info

---
+++ week-01/day-04/Variables/define-basic-info.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+
+let myName: string = 'Viktor Bertalan';
+let myAge: number = 32;
+let myHeight: number = 1.83;
+let married: boolean = true;
+
+console.log('My name is', myName);
+console.log('I\'m', myAge, 'years old.');
+console.log('I\'m', myHeight, 'meters tall.');
+console.log('I\'m married:', married);
