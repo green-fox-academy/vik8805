8b0d039d2319bbf12cb4a4d69aa46556131073da
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-11-19 13:58:34 +0100

    completed third printing exercise, hello-others

---
+++ week-01/day-04/Printing/hello-others.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+
+console.log('Hello, Martin!');
+console.log('Hello, Csaba!');
+console.log('Hello, Attila!');
