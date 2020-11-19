ea563461cb309db4115c5a0b57da07dd5f28f485
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-11-19 15:50:46 +0100

    completed the third exercise of variables, bmi

---
+++ week-01/day-04/Variables/bmi.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+
+// Print the Body mass index (BMI) based on these values
+
+let BMI: number = massInKg / heightInM ** 2;
+
+console.log('Body mass index (BMI):', BMI);
