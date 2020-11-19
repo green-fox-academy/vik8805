5f16a30390f6d9b32af7240365fd1aee145119e6
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-11-19 14:39:28 +0100

    completed second exercise of types, two-numbers

---
+++ week-01/day-04/Types/two-nmbers.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+// Create a program that prints a few operations on two numbers: 22 and 13
+
+// Print the result of 13 added to 22
+
+// Print the result of 13 substracted from 22
+
+// Print the result of 22 multiplied by 13
+
+// Print the result of 22 divided by 13 (as a decimal fraction)
+
+// Print the remainder of 22 divided by 13
+
+let i: number = 22;
+let j: number = 13;
+console.log('22 + 13 =', j + i);
+console.log('22 - 13 =', i - j);
+console.log('22 * 13 =', i * j);
+console.log('22 / 13 =', i / j);
+console.log('22 % 13 =',i % j);
