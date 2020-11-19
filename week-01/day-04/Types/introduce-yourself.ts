81094e2569f9d9ddb7e3c96521dcbc86395d15c2
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-11-19 14:29:37 +0100

    completed first exercise of types, introduce-yourself

---
+++ week-01/day-04/Types/introduce-yourself.ts
@@ -0,0 +1,22 @@
+'use strict';
+export { }
+
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+//
+//  Example output:
+//  John Doe
+//  31
+//  1.87
+
+let name: string = 'Viktor Bertalan';
+let age: number = 32;
+let height: number = 1.83;
+
+console.log('Hello! My name is', name, '!');
+console.log('I\'m', age, 'years old.');
+console.log('My height is', height, 'm.');
+
