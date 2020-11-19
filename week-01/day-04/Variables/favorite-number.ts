1bc3d84e6d44827f4b94557587a4ae2e990bcbc4
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-11-19 15:24:18 +0100

    completed first exercise of variables, favorite-numbers

---
+++ week-01/day-04/Variables/favorite-number.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+// Store your favorite number in a variable (as a number)
+// And print it like this: 'My favorite number is: 8'
+
+let favoriteNumber: number = 72;
+
+console.log('My favorite number is:', favoriteNumber);
