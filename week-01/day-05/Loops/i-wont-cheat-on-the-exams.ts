28bd4ab620a5d449e6ba89b1365ecb13535b07b2
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-11-20 11:15:36 +0100

    completed first exercise of Loops, i-wont-cheat-on-the-exams

---
+++ week-01/day-05/Loops/i-wont-cheat-on-the-exams.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+for (let i = 0, j = 1; i < 100; i++, j++) {
+  console.log(j, 'I won\'t cheat on the exam!');
+}
