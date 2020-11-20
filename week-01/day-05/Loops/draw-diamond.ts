799ae4783b31ac077af048c7bae151ae8ce7a01d
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-11-20 23:37:29 +0100

    rewrote this exercise with a better solution

--- week-01/day-05/Loops/draw-diamond.ts
+++ week-01/day-05/Loops/draw-diamond.ts
@@ -17,32 +17,29 @@ let lineCount: number = 7;
 //
 // The diamond should have as many lines as lineCount is
 
-for (let i: number = 0, pen: string = '', space: string = '', k: number = 0; i < lineCount * 2; i++) {
-  space = '';
-  pen = '';
-  if (i <= lineCount) {
-    for (let j: number = 0; j < lineCount - k; j++) {
-      space += ' ';
+for (let i: number = 0, string: string = ''; i < lineCount; i++) {
+  string = '';
+  if (i < (lineCount + 1) / 2) {
+    for (let j: number = 0; j < (lineCount + 1) / 2 + i; j++) {
+      if (j < (lineCount + 1) / 2 - (i + 1)) {
+        string += ' ';
+      } else if (j === (lineCount + 1) / 2 - (i + 1)) {
+        string += '*';
+      } else if (j > (lineCount + 1) / 2 - (i + 1)) {
+        string += '*';
+      }
     }
-    for (let l: number = 0; l < i; l++) {
-      pen += '*';
-    }
-    if (i % 2 === 1) {
-      console.log(space, pen);
-      k += 1;
-    }
+    console.log(string);   
   } else {
-    space = '';
-    pen = '';
-    for (let j: number = 0; j < i - k; j++) {
-      space += ' ';
+    for (let k: number = 0; k < lineCount + (lineCount - 1) / 2 - i; k++) {
+      if (k < i - (lineCount - 1) / 2) {
+        string += ' ';
+      } else if (k === i - (lineCount - 1) / 2) {
+        string += '*';
+      } else if (k > i - (lineCount - 1) / 2) {
+        string += '*';
+      }
     }
-    for (let l: number = 0; l < lineCount * 2 - i; l++) {
-      pen += '*';
-    }
-    if (i % 2 === 1) {
-      console.log(space, pen);
-      k += 1;
-    }
+    console.log(string);
   }
 }
