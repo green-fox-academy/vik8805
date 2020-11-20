53f1be59bd963c15bc45656a3ed15c968eb6521b
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-11-20 15:20:19 +0100

    made a for loop for the pen variable

--- week-01/day-05/Loops/draw-pyramid.ts
+++ week-01/day-05/Loops/draw-pyramid.ts
@@ -2,7 +2,7 @@
 
 export { }
 
-let lineCount: number = 4;
+let lineCount: number = 9;
 
 // Write a program that draws a
 // pyramid like this:
@@ -16,12 +16,15 @@ let lineCount: number = 4;
 
 for (let i: number = 0, pen: string = '', space: string = '', k: number = 0; i < lineCount * 2; i++) {
   for (let j: number = 0; j < lineCount - k; j++) {
-  space += ' ';
+    space += ' ';
+  }
+  for (let l: number = 0; l < i; l++) {
+    pen += '*';
   }
   if (i % 2 === 1) {
     console.log(space, pen);
     k += 1;
   }
-  pen += '*';
   space = '';
+  pen = '';
 }
