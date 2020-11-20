15959eb1503749885e4cbdc910dc8e8a99d35cb3
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-11-20 12:56:51 +0100

    completed the 6th exercise of loops, draw-triangles

---
+++ week-01/day-05/Loops/draw-triangle.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+let lineCount: number = 4;
+
+// Write a program that draws a triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as lineCount is
+
+for (let i: number = 0, pen: string = ''; i < lineCount + 1; i++) {
+  console.log(pen);
+  pen += '*';
+}
