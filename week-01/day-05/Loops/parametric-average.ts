1da7ac44753242a8f7fe09cd04fe26ca37128e81
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-11-20 12:07:09 +0100

    completed the forth loop exercise, parametric-average

---
+++ week-01/day-05/Loops/parametric-average.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+// Write a program that calculates the sum and the average from 1 to a given number.
+// Example input: 5
+// Example output: Sum: 15, Average: 3
+
+let givenNumber: number = 5;
+let sum: number = 0;
+let average: number = 0;
+
+for (let i: number = 0, j: number = givenNumber + 1; i < j; i++) {
+  sum += i;
+  if (i < givenNumber) {
+    average += i;
+  } else if (i = givenNumber) {
+    average = (average + i) / givenNumber;
+  }
+}
+console.log('Sum:', sum);
+console.log('Average:', average);
+
