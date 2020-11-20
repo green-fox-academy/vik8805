d522e42f2036eb3a2bfe74ddfaebf68d1bc2cafc
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-11-20 17:01:08 +0100

    completed the draw-square exercise

---
+++ week-01/day-05/Loops/draw-square.ts
@@ -0,0 +1,31 @@
+'use strict';
+
+let lineCount: number = 6;
+
+// Write a program that draws a square like this:
+//
+// %%%%%%
+// %    %
+// %    %
+// %    %
+// %    %
+// %%%%%%
+//
+// The square should have as many lines as lineCount is
+
+for (let i: number = 0, str: string = ''; i < lineCount; i++) {
+  str = '';
+  if (i === 0 || i === lineCount - 1) {
+    for (let j: number = 0; j < lineCount; j++) {
+      str += '%';
+    }
+    console.log(str);
+  } else {
+    str += '%';
+    for (let k: number = 0; k < lineCount - 2; k++) {
+      str += ' ';
+    }
+    str += '%';
+    console.log(str);
+  }
+}
