94995567852797cb1077183118a07015cdefa7b1
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-11-23 15:54:18 +0100

    completed the 2nd function exercise, greet

---
+++ week-02/day-01/Functions/greet.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+// -  Create variable named `nameToGreet` and assign the value `Green Fox` to it
+// -  Create a function called `greet` that greets it's input parameter
+//     -  Greeting is printing e.g. `Greetings, dear Green Fox`
+// -  Greet `nameToGreet`
+
+let nameToGreet: string = 'Green Fox';
+
+function greet(inputStr: string): void {
+  console.log('Greetings, dear', inputStr);
+}
+
+greet(nameToGreet);
