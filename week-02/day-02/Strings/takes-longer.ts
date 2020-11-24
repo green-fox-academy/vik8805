b108f43fef819e2d76269362f1c7d3e693127a26
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-11-24 16:08:12 +0100

    completed te 4th string exercise, takes longer

---
+++ week-02/day-02/Strings/takes-longer.ts
@@ -0,0 +1,9 @@
+'use strict';
+// While saving this quote, a disk error has occured. Please fix it.
+// Add "always takes longer than" between the words "It" and "you"
+
+let quote: string = `Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.`;
+
+quote = `${quote.slice(0, 21)}always takes longer than ${quote.slice(21)}`;
+
+console.log(quote);
