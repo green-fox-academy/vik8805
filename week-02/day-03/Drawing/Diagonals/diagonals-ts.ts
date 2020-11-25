dd0ca67dc3a4ad29bd4dceaa82a7c572d8023f6c
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-11-25 14:13:22 +0100

    completed the 3rd exercise of drawing, diagonals

---
+++ week-02/day-03/Drawing/Diagonals/diagonals-ts.ts
@@ -0,0 +1,40 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+export {};
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Draw the canvas' diagonals.
+// If it starts from the upper-left corner it should be green, otherwise it should be red.
+
+function drawDiagonal(startHorizontal: number, startVertical: number) {
+  if (startHorizontal === 0 && startVertical === 0) {
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(canvas.width, canvas.height);
+    ctx.strokeStyle = 'green';
+    ctx.stroke();
+  } else if (startHorizontal === 0 && startVertical === canvas.height) {
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height);
+    ctx.lineTo(canvas.width, 0);
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+  } else if (startHorizontal === canvas.width && startVertical === canvas.height) {
+    ctx.beginPath();
+    ctx.moveTo(canvas.width, canvas.height);
+    ctx.lineTo(0, 0);
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+  } else {
+    ctx.beginPath();
+    ctx.moveTo(canvas.width, 0);
+    ctx.lineTo(0, canvas.height);
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+  };
+};
+
+drawDiagonal(0, 0);
