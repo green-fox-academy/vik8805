b103b1683aa432b48f893018cf8c29b5bf5dfc80
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-11-25 17:52:17 +0100

    completed the 5th exercise of drawing, horizontal

---
+++ week-02/day-03/Drawing/Horizontal/horizontal-lines-ts.ts
@@ -0,0 +1,36 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+export {};
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Create a function that draws a single line and takes 2 parameters:
+// the x and y coordinates of the line's starting point
+// and draws a 50 long horizontal line from that point.
+// Draw at least 3 lines with that function using a loop.
+
+function drawHorizLine(x: number, y: number) {
+  if (x + 50 >= canvas.width) {
+    alert(`You have to give a smaller number than ${canvas.width - 50} to x.`);
+  } else if (y >= canvas.height / 2) {
+      for (let i: number = 0; i < 3; i++) {
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.lineTo(x + 50, y);
+      ctx.stroke();
+      y -= canvas.height / 6;
+      };
+    } else {
+      for (let i: number = 0; i < 3; i++) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x + 50, y);
+        ctx.stroke();
+        y += canvas.height / 6
+      };
+    };
+};
+
+drawHorizLine(400, 200);
