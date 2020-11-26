62da3648974a83c886cdf13fa4958e441fc43e4a
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-11-26 14:27:10 +0100

    completed the checkerboard exercise of drawing

---
+++ week-02/day-04/Drawing/Checkerboard/checkerboard-ts.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+export {};
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Fill the canvas with a checkerboard pattern.
+
+function drawSquare(startX: number, startY: number, color: string) {
+  ctx.fillStyle = color;
+  ctx.fillRect(startX, startY, canvas.width / 8, canvas.height / 8)
+};
+
+for (let i = 0, startI: number = 0; i < 8; i++) {
+  for (let j = 0, startJ: number = 0; j < 8; j++) {
+    if (j % 2 === i % 2) {
+      drawSquare(startJ, startI, 'black');
+    } else {
+      drawSquare(startJ, startI, 'red');
+    };
+    startJ += canvas.width / 8;
+  };
+  startI += canvas.height / 8;
+};
