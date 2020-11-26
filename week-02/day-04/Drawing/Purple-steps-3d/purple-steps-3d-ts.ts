b54dfecbfaf75cc58b9d119a0a1b4dae22abfb97
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-11-26 13:35:04 +0100

    completed Purple steps 3d exercise of drawing

---
+++ week-02/day-04/Drawing/Purple-steps-3d/purple-steps-3d-ts.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+export {};
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Reproduce this:
+// [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/assets/r4.png]
+
+function drawPurpleSteps3d(start: number, size: number) {
+  for (let i = 0; i < 6; i++) {
+    ctx.strokeRect(start, start, size, size);
+    ctx.fillStyle = '#b04ef0';
+    ctx.fillRect(start + 1, start + 1, size - 2, size - 2);
+    start += size;
+    size += 10;
+  };
+};
+
+drawPurpleSteps3d(10, 10);
