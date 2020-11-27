edb3d1cde63728797bbf3af373f66e2c597fb2d7
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-11-27 13:06:39 +0100

    completed the 4th drawing project, Triangles

---
+++ week-02/day-05/Triangles/triangles-ts.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+export {};
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+function drawLTriangle(startX: number, startY: number, sideSize: number) {
+  ctx.beginPath();
+  ctx.moveTo(startX, startY);
+  ctx.lineTo(startX + sideSize, startY);
+  ctx.lineTo(startX + sideSize - sideSize / 2, startY - (0.5 * Math.sqrt(3) * sideSize));
+  ctx.lineTo(startX, startY);
+  ctx.stroke();
+};
+
+let side: number = canvas.width / 21;
+
+for (let h = 0; h < 21; h++) {
+  for (let i = 0; i < 21 - h; i++) {
+    drawLTriangle(side / 2 * h + side * i, canvas.height - (0.5 * Math.sqrt(3) * side) * h, side);
+  };
+};
