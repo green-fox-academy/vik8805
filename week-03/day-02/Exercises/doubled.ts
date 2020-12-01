aff909f89f4170aac58dd54f708aa3a2d486fdfc
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-12-01 23:55:09 +0100

    completed the Doubled exercise

---
+++ week-03/day-02/Exercises/doubled.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+// Create a method that decrypts duplicated-chars.txt
+
+const fs = require('fs');
+let decryptedText: string = '';
+
+try {
+  let fileContent: string = fs.readFileSync('duplicated-chars.txt', 'utf-8');
+  for (let i = 0; i < fileContent.length; i++) {
+    if (i % 2) {
+      decryptedText += fileContent[i];
+    };
+  };
+  console.log(decryptedText);
+}
+catch(e) {
+    console.log(e);
+};
