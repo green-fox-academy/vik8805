a8fb838328b6df62d847f72d5d0107fd3b4c194b
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-12-02 00:45:36 +0100

    completed the Reversed lines exercise

---
+++ week-03/day-02/Exercises/reversed-lines.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+// Create a method that decrypts reversed-lines.txt
+
+const fs = require('fs');
+let decryptedText: string = '';
+
+try {
+  let fileContent: string[] = fs.readFileSync('reversed-lines.txt', 'utf-8').toString().split('\r\n');
+  for (let i: number = 0; i < fileContent.length; i++) {
+    decryptedText += `${fileContent[i].split('').reverse().join('')}\r\n`;
+  };
+  console.log(decryptedText);
+}
+catch(e) {
+    console.log(e);
+};
