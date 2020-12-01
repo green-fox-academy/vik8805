3fdbd578e01a659f4cde527c0b5e6bbe8b5aeda9
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-12-01 15:52:20 +0100

    completed divide by zero with try catch

---
+++ week-03/day-02/Exceptions/divided-by-zero-with-try-catch.ts
@@ -0,0 +1,23 @@
+'use strict';
+
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print 'fail' if the parameter is 0
+
+function divideByZero(inputNumber: number): void {
+  let result: number = 0;
+  try {
+    if (inputNumber === 0) {
+      throw 'fail'; 
+    } else {
+      result = 10 / inputNumber;
+      return console.log(result);
+    };
+  }
+  catch(e) {
+    return console.log(e);
+  };
+};
+
+divideByZero(0);
