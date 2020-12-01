3c057fef9ecd96ab00f2e5647a8b560eef8f811a
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-12-01 17:42:00 +0100

    completed the Copy file exercise

---
+++ week-03/day-02/Writing-files/copy-file.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+// Write a function that copies the contents of a file into another
+// It should take the filenames as parameters
+// It should return a boolean that shows if the copy was successful
+
+function copyFile(copyFrom: string, copyInto: string): boolean {
+  const fs = require('fs');
+  try {
+    let copyFromContent: string = fs.readFileSync(copyFrom, 'utf-8');
+    fs.writeFileSync(copyInto, copyFromContent);
+  }
+  catch(e) {
+    return false;
+  };
+  return fs.readFileSync(copyFrom, 'utf-8') === fs.readFileSync(copyInto, 'utf-8');
+};
+
+console.log(copyFile('innen.txt', 'ide.txt'));
