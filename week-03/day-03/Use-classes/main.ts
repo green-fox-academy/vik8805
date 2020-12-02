e40ca64b139d11b3f30249035d435f0731f0bdfb
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-12-02 17:38:21 +0100

    completed the Pokemon exercise

---
+++ week-03/day-03/Use-classes/main.ts
@@ -0,0 +1,36 @@
+'use strict';
+
+import { Pokemon } from './Pokemon'
+
+let pokemonOfAsh: Pokemon[] = initializePokemon();
+
+// Every pokemon has a name and a type.
+// Certain types are effective against others, e.g. water is effective against fire.
+
+// Ash has a few pokemon.
+// A wild pokemon appeared!
+
+let wildPokemon: Pokemon = new Pokemon('Oddish', 'leaf', 'water');
+
+// Which pokemon should Ash use?
+
+// Implement the following `chooseEffective` function which will choose the most effective pokemon for Ash
+console.log(`I choose you!, ${chooseEffective(pokemonOfAsh, wildPokemon)}`);
+
+function initializePokemon(): Pokemon[] {
+    return [
+        new Pokemon('Balbasaur', 'leaf', 'water'),
+        new Pokemon('Pikatchu', 'electric', 'water'),
+        new Pokemon('Charizard', 'fire', 'leaf'),
+        new Pokemon('Balbasaur', 'water', 'fire'),
+        new Pokemon('Kingler', 'water', 'fire')
+    ];
+};
+
+function chooseEffective(myPokemons: Pokemon[], hostilePokemon: Pokemon){
+  for (let i: number = 0; i < myPokemons.length; i++) {
+    if (myPokemons[i].isEffectiveAgainst(hostilePokemon)) {
+      return myPokemons[i].name;
+    };
+  };
+};
