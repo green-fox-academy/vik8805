2c59891df2e88c5a14c781dc39314134e1c558e2
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-12-03 23:32:10 +0100

    completed factorio exercise with recursion

--- week-02/day-01/Functions/factorio.ts
+++ week-02/day-01/Functions/factorio.ts
@@ -2,13 +2,22 @@
 
 // -  Create a function called `factorio`
 //    that returns it's input's factorial
-
+/*
 function factorio(inputNumber: number): number {
   let factorialOfNumber: number = 1;
   for (let i: number = inputNumber; i > 1; i--) {
     factorialOfNumber *= i;
   }
   return factorialOfNumber;
+}*/
+
+// With recursion
+function factorio(inputNumber: number): number {
+  if (inputNumber === 0) {
+    return 1;
+  } else {
+    return inputNumber * factorio(inputNumber - 1);
+  }
 }
 
 console.log(factorio(5));
