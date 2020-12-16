ec5d3782197bd5207175c8951ffd5245452b62d4
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-12-16 15:29:02 +0100

    completed the Double items exercise of Arrays

---
+++ week-02/day-01/Arrays/double-items.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+// - Create an array variable named `drinks` with the following content:
+//   `['Gin', 'Whiskey', 'Wine', 'Beer']`
+// - Double all the strings in the array, use a built in array method instead of
+//   a loop
+// - It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
+
+let drinks: string[] = ['Gin', 'Whiskey', 'Wine', 'Beer'];
+
+drinks.forEach(function (value: string, index: number) {
+  drinks[index] += drinks[index];
+});
+
+console.log(drinks);
