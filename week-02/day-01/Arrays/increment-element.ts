ac0eb98a1cbcef1e27ffa53b7454481c0882c656
Viktor Bertalan <bertalan.viktor@gmail.com>
2020-12-16 12:36:42 +0100

    completed the Increment element exercise in Arrays

---
+++ week-02/day-01/Arrays/increment-element.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// -  Create a variable named `numbers` with the following content: `[1, 2, 3, 4, 5]`
+// -  Increment the third element directly (without loops and without array methods: forEach, map, ...)
+// -  Log the third element to the console
+
+let numbers: number[] = [1, 2, 3, 4, 5];
+
+numbers[2]++;
+
+console.log(numbers[2]);
