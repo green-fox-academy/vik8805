e72e907daf226d71b78c255192c86ca073d6da20
Viktor Bertalan <bertalan.viktor@gmail.com>
2021-01-08 14:25:18 +0100

    created the image store, filled it with pictures from gallery database

--- week-06/day-05/Gallery/index.js
+++ week-06/day-05/Gallery/index.js
@@ -1,10 +1,11 @@
-'use trict';
+'use strict';
+
+let gallery = ['clouds', 'clower', 'lake', 'rails', 'landscape', 'space', 'sunset', 'travel', 'water', 'waves',];
 
 const newBackground = document.createElement('div');
 const body = document.querySelector('body');
 body.insertBefore(newBackground, body.childNodes[0]);
 newBackground.classList.add('background');
-
 
 const newMain = document.createElement('div');
 const background = document.querySelector('.background');
@@ -16,11 +17,11 @@ const main = document.querySelector('.main');
 main.appendChild(newControlPanel);
 newControlPanel.classList.add('controlPanel');
 
-const newImageBox = document.createElement('img');
+const newActualImage = document.createElement('img');
 const controlPanel = document.querySelector('.controlPanel');
-controlPanel.appendChild(newImageBox);
-newImageBox.classList.add('actualImage');
-newImageBox.setAttribute('src', 'images/waves.jpg');
+controlPanel.appendChild(newActualImage);
+newActualImage.classList.add('actualImage');
+newActualImage.setAttribute('src', 'images/waves.jpg');
 
 for (let i = 0; i < 2; i++) {
   const newArrowImage = document.createElement('img');
@@ -39,3 +40,18 @@ for (let i = 0; i < 2; i++) {
 const newImageStore = document.createElement('div');
 main.appendChild(newImageStore);
 newImageStore.classList.add('imageStore');
+
+for (let j = 0; j < gallery.length; j++) {
+  if (j < 8) {
+    const newSmallImageBox = document.createElement('div');
+    const imageStore = document.querySelector('.imageStore');
+    imageStore.appendChild(newSmallImageBox);
+    newSmallImageBox.classList.add(`smallImageBox`);
+    newSmallImageBox.classList.add(`${j}`);
+    const newSmallImage = document.createElement('img');
+    const smallImageBox = document.querySelectorAll(`.smallImageBox`);
+    smallImageBox[j].appendChild(newSmallImage);
+    newSmallImage.classList.add('smallImage');
+    newSmallImage.setAttribute('src', `images/${gallery[j]}.jpg`);
+  }
+}
