83e48ba9bfdbf4cbcda772c1f18f04c1f85c51be
Viktor Bertalan <bertalan.viktor@gmail.com>
2021-01-12 12:28:16 +0100

    on get require index.html loaded

--- week-07/day-01/app.js
+++ week-07/day-01/app.js
@@ -2,9 +2,10 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.get('/', (req, res) => {
-  res.send('OK!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(3000);
