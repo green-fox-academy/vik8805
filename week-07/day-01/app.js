e5d402dbba1ffce977b6542155d6e221c9caf946
Viktor Bertalan <bertalan.viktor@gmail.com>
2021-01-12 12:39:36 +0100

    completed basic backend exercise, app is running without errors

--- week-07/day-01/app.js
+++ week-07/day-01/app.js
@@ -1,8 +1,11 @@
 'use strict';
 
 const express = require('express');
-const app = express();
 const path = require('path');
+
+const app = express();
+
+app.use(express.static('assets'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
