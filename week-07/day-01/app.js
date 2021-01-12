75178eb44fa8e37ae37cce11a41a945ffd962db9
Viktor Bertalan <bertalan.viktor@gmail.com>
2021-01-12 13:07:17 +0100

    completed doubling exercise, clicking try doubling endpoint provide successful responses

--- week-07/day-01/app.js
+++ week-07/day-01/app.js
@@ -11,4 +11,21 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/doubling', (req, res) => {
+  let received = req.query.input;
+  if (received === undefined) {
+    let errorMessage = {
+      error: "Please provide an input!"
+    };
+    res.status(200).json(errorMessage);
+  } else {
+    let result = received * 2;
+    let doublingResponse = {
+      received: Number(received), 
+      result: result,
+    };
+    res.json(doublingResponse);
+  };
+});
+
 app.listen(3000);
