48a7069d4d3b69150bf954a70e0ad6ffd418b51f
Viktor Bertalan <bertalan.viktor@gmail.com>
2021-01-16 23:05:46 +0100

    connected app to database, endpoint created to list posts from reddit database

--- week-07/day-05/Reddit/server.js
+++ week-07/day-05/Reddit/server.js
@@ -25,4 +25,14 @@ app.get('/hello', (req, res) => {
   res.send('hello world');
 });
 
+app.get('/posts', (req, res) => {
+  conn.query('SELECT * FROM posts', (err, rows) => {
+    if (err) {
+      res.status(500).json(err);
+      return;
+    };
+    res.status(200).json(rows);
+  });
+});
+
 app.listen(3000);
