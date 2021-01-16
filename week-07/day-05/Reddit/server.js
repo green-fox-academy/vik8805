dabb3b4c05beb8e692de6e4dfd6b704755e90b76
Viktor Bertalan <bertalan.viktor@gmail.com>
2021-01-16 22:38:50 +0100

    reddit database with posts table and it's connection created

--- week-07/day-05/Reddit/server.js
+++ week-07/day-05/Reddit/server.js
@@ -2,6 +2,22 @@
 
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1988',
+  database: 'reddit',
+  insecureAuth: 'true',
+});
+
+conn.connect((err) => {
+  if(err) {
+    console.log(err.toString());
+  }
+  console.log('Connected to mysql');
+});
 
 app.use(express.json());
 
