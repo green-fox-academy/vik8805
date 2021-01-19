cbb859f28d0d66aeb3cb8744b869b89ed15e09c6
Viktor Bertalan <bertalan.viktor@gmail.com>
2021-01-19 14:52:45 +0100

    completed the one time clicker exercise

---
+++ week-08/day-02/One-Time-Clicker/one-time-clicker.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Create an event listener which logs the current timestamp to the console once the button is clicked.
+// When the button is clicked for the second or any other times no event listener should be called.
+// Try to solve this two different ways.
+// hint: take a look at the button attributes https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button
+
+const button = document.querySelector('button');
+
+// First solution
+button.addEventListener('click', () => {
+  const currentTimestamp = new Date().getTime();
+  console.log(currentTimestamp);
+  button.setAttribute('disabled', true);
+});
+
+// Second solution
+let counter = 0;
+button.addEventListener('click', () => {
+  if (counter < 1) {
+    const currentTimestamp = new Date().getTime();
+    console.log(currentTimestamp);
+    counter++;
+  };
+});
